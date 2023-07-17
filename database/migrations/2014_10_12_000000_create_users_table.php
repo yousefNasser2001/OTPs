bc74782bc0e43da6f104929e9024f6553116ed66
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-07-17 15:43:55 +0300

    add two coulmn to users_table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('code')->nullable();
+            $table->dateTime('expire_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
