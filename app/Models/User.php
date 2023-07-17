4650db8004d399699d81e061bd1c80890c55c1ff
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-07-17 15:42:29 +0300

    add two coulmn to users_table

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'code',
+        'expire_at',
     ];
 
     /**
@@ -42,4 +44,21 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function generateCode()
+    {
+        $this->timestamps = false;
+        $this->code = rand(1000 , 9999);
+        $this->expire_at = now()->addMinute(15);
+        $this->save();
+    }
+
+    public function resetCode()
+    {
+        $this->timestamps = false;
+        $this->code = null;
+        $this->expire_at = null;
+        $this->save();
+    }
 }
